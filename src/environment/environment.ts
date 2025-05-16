9f5bcdd76df92bc709967200e0ce80765c48c2ff
prathmkorgaonkar <prathmkorgaonkar@gmail.com>
2025-05-16 13:53:09 +0530

    update environment file

---
+++ src/environment/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  BACKEND_SERVER_URL: 'https://auth-backend-ri8p.onrender.com' // your local backend URL
+};
+
+// export const environment = {
+//   production: false,
+//   BACKEND_SERVER_URL: 'http://localhost:3001' // your local backend URL
+// };
